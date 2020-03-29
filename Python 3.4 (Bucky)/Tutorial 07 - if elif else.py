d2446e1fa728a2a069c4a57e3d2b63fa34ef0117
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2020-03-28 23:21:23 -0400

    Updated tutorial 05. Finished tutorial 07.

--- Python 3.4 (Bucky)/Tutorial 07 - if elif else.py
+++ Python 3.4 (Bucky)/Tutorial 07 - if elif else.py
@@ -1,3 +1,14 @@
-print ("Hi there")
-print(1)
-print(2)
+# Compare Numbers
+age = 13
+if age < 21:
+    print("No beer for you!")
+
+# Compare Strings
+name = "Lucy"
+if name == "Bucky":
+    print("Hey there Bucky")
+elif name == "Lucy":
+    print("What's up Lucy?")
+else:
+    print("Please sign up for the site!")
+
