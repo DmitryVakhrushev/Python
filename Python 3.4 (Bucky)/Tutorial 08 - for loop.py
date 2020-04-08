4ab0bf7a71c8032f8af5d9b23ece24d85ca89b63
Dmitry Vakhrushev <dm.vakhrushev@gmail.com>
2020-04-07 23:18:07 -0400

    For loop tutorial.

---
+++ Python 3.4 (Bucky)/Tutorial 08 - for loop.py
@@ -0,0 +1,11 @@
+
+foods = ['bacon','tuna','ham','sausages','beef']
+
+for foodItem in foods:
+    print(foodItem)
+    print(len(foodItem))
+
+print('----------------------')
+# Loop through a part of the list
+for foodItem in foods[:2]:
+    print(foodItem)
